4701e3c608d5406690877d2457f9a18b40462677
AhirwarDevendra <onlytoit.da@gmail.com>
2016-07-29 20:47:22 +0530

    1st commit

---
+++ www/js/app.js
@@ -0,0 +1,125 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider,$urlRouterProvider,$ionicConfigProvider,$sceDelegateProvider){
+    
+$stateProvider
+  .state('gallery',{
+    url:'/gallery',
+    templateUrl:'templates/gallerydemo.html',
+    controller:'MediaCtrl'
+  })
+   
+    .state('sharing',{
+    url:'/sharing',
+    templateUrl:'templates/sharingdemo.html',
+    controller:'ShareCtrl'
+  })
+    
+  .state('chat',{
+    url:'/chat',
+    templateUrl:'templates/chatdemo.html',
+    controller:'ChatCtrl'
+})
+
+$urlRouterProvider.otherwise('/sharing');
+})
+
+.controller('MediaCtrl', function($scope, $ionicModal) {
+	$scope.allImages = [{
+		'src' : 'http://dreamwood.in/images/team/Devendra.jpg'
+	}, {
+		'src' : 'http://dreamwood.in/images/team/Swapnil.jpg'
+	}, {
+		'src' : 'http://dreamwood.in/images/team/arun.jpg'
+	}, {
+		'src' : 'http://dreamwood.in/images/team/Swapnil.jpg'
+	}, {
+		'src' : 'http://dreamwood.in/images/team/Swapnil.jpg'
+	}, {
+		'src' : 'http://dreamwood.in/images/team/Swapnil.jpg'
+	}, {
+		'src' : 'http://dreamwood.in/images/team/Swapnil.jpg'
+	}, {
+		'src' : 'http://dreamwood.in/images/team/Swapnil.jpg'
+	}];
+ 
+	$scope.showImages = function(index) {
+		$scope.activeSlide = index;
+		$scope.showModal('templates/image-popover.html');
+	}
+ 
+	$scope.showModal = function(templateUrl) {
+		$ionicModal.fromTemplateUrl(templateUrl, {
+			scope: $scope,
+			animation: 'slide-in-up'
+		}).then(function(modal) {
+			$scope.modal = modal;
+			$scope.modal.show();
+		});
+	}
+ 
+	// Close the modal
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+		$scope.modal.remove()
+	};
+    
+    $scope.clipSrc = '../img/abc.mp4';
+ 
+$scope.playVideo = function() {
+	$scope.showModal('templates/video-popover.html');
+}
+})
+
+.controller('ShareCtrl', function($scope, $ionicModal,$cordovaSocialSharing){
+    console.log("Sharing");
+  
+    $scope.share = function()
+    {
+        console.log("click");
+      
+      $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+      $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+      $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+      $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+      $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "https://www.thepolyglotdeveloper.com");
+    }
+})
+
+.controller('ChatCtrl',function($scope,$ionicModal){
+  
+  
+var $_Tawk_API={},$_Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5731b2a4c6045a5c6e8cf6b2/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+})
